b48759e00720c95f36c4b1cd12dad811fb06530c
Marc Magon <ged_marc@hotmail.com>
2017-06-12 14:40:31 +0200

    first commit

---
+++ test/za/co/mmagon/jwebswing/plugins/angularroute/AngularRoutePageConfiguratorTest.java
@@ -0,0 +1,25 @@
+package za.co.mmagon.jwebswing.plugins.angularroute;
+
+import org.junit.Test;
+import za.co.mmagon.BaseTestClass;
+import za.co.mmagon.jwebswing.Page;
+
+/**
+ *
+ * @author Marc Magon
+ */
+public class AngularRoutePageConfiguratorTest extends BaseTestClass
+{
+
+    public AngularRoutePageConfiguratorTest()
+    {
+    }
+
+    @Test
+    public void testSomeMethod()
+    {
+        Page p = getInstance();
+        System.out.println(p.toString(0));
+    }
+
+}
