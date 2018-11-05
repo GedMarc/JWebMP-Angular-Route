c7a0d52a4d44229dabcd5776fb9cd1728a6260e2
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:29 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

---
+++ src/main/java/com/jwebmp/plugins/angularroute/implementations/AngularRouteExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.angularroute.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AngularRouteExclusionsModule
+		implements IGuiceScanModuleExclusions<AngularRouteExclusionsModule>,
+				           IGuiceScanJarExclusions<AngularRouteExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-angular-route-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.angularroute");
+		return strings;
+	}
+}
