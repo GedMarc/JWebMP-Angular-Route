85d39e1609cb0553b5608da6878345185578a16d
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:21 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,22 @@
+import com.jwebmp.core.base.angular.services.IAngularModule;
+import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+import com.jwebmp.plugins.angularroute.AngularRouteModule;
+import com.jwebmp.plugins.angularroute.AngularRoutePageConfigurator;
+import com.jwebmp.plugins.angularroute.implementations.AngularRouteExclusionsModule;
+
+module com.jwebmp.plugins.angularroute {
+	exports com.jwebmp.plugins.angularroute;
+
+	requires com.jwebmp.core;
+	requires java.validation;
+	requires com.jwebmp.guicedinjection;
+
+	provides IPageConfigurator with AngularRoutePageConfigurator;
+	provides IAngularModule with AngularRouteModule;
+
+	provides IGuiceScanModuleExclusions with AngularRouteExclusionsModule;
+	provides IGuiceScanJarExclusions with AngularRouteExclusionsModule;
+
+}
