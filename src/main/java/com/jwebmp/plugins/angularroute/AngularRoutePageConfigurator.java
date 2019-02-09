3c5837f5ba1439550838142a819d49629f5beaf7
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:47 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/angularroute/AngularRoutePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularroute/AngularRoutePageConfigurator.java
@@ -92,7 +92,7 @@ public class AngularRoutePageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
